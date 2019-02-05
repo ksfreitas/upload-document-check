b3d7a8c2f6f73bcd14ba831c30ec558534b3d6e8
Cássio de Freitas e Silva <ksfreitas@gmail.com>
2019-02-05 17:59:47 -0200

    Initial commit.

---
+++ test/pdf-check.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+describe('PDF File test', function () {
+    const PdfValidation = require('../lib/pdf-validation');
+
+    it('should return as valid PDF', (done) => {
+        const filePath = __dirname + '/files/pdf-valid.pdf';
+        PdfValidation.validPdf(filePath).then(() => {
+            done();
+        }).catch((e) => {
+            done(e);
+        })
+    });
+    it('should return as invalid PDF', (done) => {
+        const filePath = __dirname + '/files/pdf-only-header.pdf';
+        PdfValidation.validPdf(filePath).then(() => {
+            done(new Error('Valid PDF.'));
+        }).catch((e) => {
+            done();
+        })
+    });
+
+    // it('should return number of charachters in a string', function () {
+    //     assert.equal("Hello".length, 4);
+    // });
+    // it('should return first charachter of the string', function () {
+    //     assert.equal("Hello".charAt(0), 'H');
+    // });
+});
