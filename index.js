8f281e5af1f219bf1e63e8b6621fc318c4f5f91a
Cássio de Freitas e Silva <ksfreitas@gmail.com>
2019-02-18 17:09:43 -0300

    Solve a structural problem in promise functions.

--- index.js
+++ index.js
@@ -1,90 +1,112 @@
 module.exports = {
     validDoc: (filePath) => {
-        const DocValidation = require('./lib/doc-validation');
-        DocValidation.validDoc(filePath).then(() => {
-            return true;
-        }).catch(() => {
-            return false;
+        return new Promise((resolve) => {
+            const DocValidation = require('./lib/doc-validation');
+            DocValidation.validDoc(filePath).then(() => {
+                resolve(true);
+            }).catch(() => {
+                resolve(false);
+            });
         });
     },
     validDocx: (filePath) => {
-        const DocxValidation = require('./lib/docx-validation');
-        DocxValidation.validDocx(filePath).then(() => {
-            return true;
-        }).catch(() => {
-            return false;
+        return new Promise((resolve) => {
+            const DocxValidation = require('./lib/docx-validation');
+            DocxValidation.validDocx(filePath).then(() => {
+                resolve(true);
+            }).catch(() => {
+                resolve(false);
+            });
         });
     },
     validImage: (filePath) => {
-        const ImageValidation = require('./lib/image-validation');
-        ImageValidation.validImage(filePath).then(() => {
-            return true;
-        }).catch(() => {
-            return false;
-        })
+        return new Promise((resolve) => {
+            const ImageValidation = require('./lib/image-validation');
+            ImageValidation.validImage(filePath).then(() => {
+                resolve(true);
+            }).catch(() => {
+                resolve(false);
+            })
+        });
     },
     validPdf: (filePath) => {
-        const PdfValidation = require('./lib/pdf-validation');
-        PdfValidation.validPdf(filePath).then(() => {
-            return true;
-        }).catch(() => {
-            return false;
-        })
+        return new Promise((resolve) => {
+            const PdfValidation = require('./lib/pdf-validation');
+            PdfValidation.validPdf(filePath).then(() => {
+                resolve(true);
+            }).catch(() => {
+                resolve(false);
+            })
+        });
     },
     validPpt: (filePath) => {
-        const PptValidation = require('./lib/ppt-validation');
-        PptValidation.validPpt(filePath).then(() => {
-            return true;
-        }).catch(() => {
-            return false;
-        })
+        return new Promise((resolve) => {
+            const PptValidation = require('./lib/ppt-validation');
+            PptValidation.validPpt(filePath).then(() => {
+                resolve(true);
+            }).catch(() => {
+                resolve(false);
+            })
+        });
     },
     validPptx: (filePath) => {
-        const PptxValidation = require('./lib/pptx-validation');
-        PptxValidation.validPptx(filePath).then(() => {
-            return true;
-        }).catch(() => {
-            return false;
-        })
+        return new Promise((resolve) => {
+            const PptxValidation = require('./lib/pptx-validation');
+            PptxValidation.validPptx(filePath).then(() => {
+                resolve(true);
+            }).catch(() => {
+                resolve(false)
+            })
+        });
     },
     validRar: (filePath) => {
-        const RarValidation = require('./lib/rar-validation');
-        RarValidation.validRar(filePath).then(() => {
-            return true;
-        }).catch(() => {
-            return false;
-        })
+        return new Promise((resolve) => {
+            const RarValidation = require('./lib/rar-validation');
+            RarValidation.validRar(filePath).then(() => {
+                resolve(true);
+            }).catch(() => {
+                resolve(false);
+            })
+        });
     },
     validTxt: (filePath) => {
-        const TxtValidation = require('./lib/txt-validation');
-        TxtValidation.validTxtPlain(filePath).then(() => {
-            return true;
-        }).catch(() => {
-            return false;
-        })
+        return new Promise((resolve) => {
+            const TxtValidation = require('./lib/txt-validation');
+            TxtValidation.validTxtPlain(filePath).then(() => {
+                resolve(true);
+            }).catch(() => {
+                resolve(false);
+            })
+        });
     },
     validXls: (filePath) => {
-        const XlsValidation = require('./lib/xls-validation');
-        XlsValidation.validXlsx(filePath).then(() => {
-            return true;
-        }).catch(() => {
-            return false;
-        })
+        return new Promise((resolve) => {
+            const XlsValidation = require('./lib/xls-validation');
+            XlsValidation.validXlsx(filePath).then(() => {
+                resolve(true);
+            }).catch(() => {
+                resolve(false);
+            })
+        });
     },
     validXlsx: (filePath) => {
-        const XlsxValidation = require('./lib/xlsx-validation');
-        XlsxValidation.validXlsx(filePath).then(() => {
-            return true;
-        }).catch(() => {
-            return false;
-        })
+        return new Promise((resolve) => {
+            const XlsxValidation = require('./lib/xlsx-validation');
+            XlsxValidation.validXlsx(filePath).then(() => {
+                resolve(true);
+            }).catch(() => {
+                resolve(false);
+            })
+        });
     },
     validZip: (filePath) => {
-        const ZipValidation = require('./lib/zip-validation');
-        ZipValidation.validZip(filePath).then(() => {
-            return true;
-        }).catch(() => {
-            return false;
-        })
+        return new Promise((resolve) => {
+            const ZipValidation = require('./lib/zip-validation');
+            ZipValidation.validZip(filePath).then(() => {
+                resolve(true);
+            }).catch(() => {
+                resolve(false);
+            })
+        });
     }
 };
